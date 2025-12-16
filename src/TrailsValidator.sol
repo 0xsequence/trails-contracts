a199a54d91dd82dd212afdd586b33a1d40c10a63
Shun Kakinoki <shunkakinoki@gmail.com>
2025-12-16 19:39:08 +0900

    chore: Update comment to clarify porting of RequireUtils to TrailsValidator

--- src/TrailsValidator.sol
+++ src/TrailsValidator.sol
@@ -7,6 +7,7 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 // From: https://github.com/0xsequence/wallet-contracts/blob/db6789c3f8ad774dc55253f0599e0f2f0833f76a/contracts/modules/utils/RequireUtils.sol
+// Ported `RequireUtils` to `TrailsValidator` with modifications to use msg.sender
 
 /**
  * @title TrailsValidator
