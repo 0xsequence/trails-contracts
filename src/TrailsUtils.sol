b3124f45a3f9eee624ef99c6109e6b3d405e98c8
Michael Standen <mstan@sequence.xyz>
2025-12-17 10:35:55 +1300

    Add SequenceDelegate

--- src/TrailsUtils.sol
+++ src/TrailsUtils.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.27;
 import {MalleableSapient} from "src/modules/MalleableSapient.sol";
 import {SharedProxy} from "src/modules/SharedProxy.sol";
 import {RequireUtils} from "src/modules/RequireUtils.sol";
+import {SequenceDelegate} from "src/modules/SequenceDelegate.sol";
 
 // Combine all tools in one so we don't have to touch different addresses when processing an intent.
-contract TrailsUtils is MalleableSapient, SharedProxy, RequireUtils { }
+contract TrailsUtils is MalleableSapient, SharedProxy, RequireUtils, SequenceDelegate {}
